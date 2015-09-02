b017fde7b21535660818c13185e3cb96de163c70
Lacey Rogers <lacey.rogers@domo.com>
2015-09-02 10:15:40 -0600

    Lacey's first commit

--- DFS.cs
+++ DFS.cs
@@ -2,9 +2,13 @@ using System;
 namespace a1{
   public class DepthFirstSearch : Algorithm{
 
+
 public override void Execute(){
   throw new NotImplementedException();
+  Console.WriteLine("Test");
 }
+
+      //implement depth first search
     
   }
 }
